50b3c25c7128577ca7c4494f3b412eb275f845e5
Nikita <ashtonashtonkachur@gmail.com>
2023-01-25 21:12:51 -0500

    add selection sort function"

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-arr = [8, 11, -1, 24, 0, 3, 2]
+arr = [8, 11, -1, 24, 0, 3, 2, -100]
+
+
+def selection_sort(array):
+    for i in range(len(array) - 1):
+        min_value = array[i]
+        index = i
+        for j in range(i + 1, len(array)):
+            if min_value > array[j]:
+                min_value = array[j]
+                index = j
+
+        if index != i:
+            # old_value
+            temporary_var = array[i]
+            array[i] = array[index]
+            array[index] = temporary_var
+    return array
