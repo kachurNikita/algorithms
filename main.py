81ccb1a9558b0fbe3c8f25f86746d3045b283d84
Nikita <ashtonashtonkachur@gmail.com>
2023-01-27 10:55:01 -0500

    bubble_sort algorithm add

--- main.py
+++ main.py
@@ -16,3 +16,21 @@ def selection_sort(array):
             array[i] = array[index]
             array[index] = temporary_var
     return array
+
+
+
+
+bubble_arr = [1, -1, 5, 2, 9, 4]
+
+def bubble_sort(array):
+    n = len(array)
+    for i in range(n - 1):
+        for j in range(n - 1):
+            if array[j + 1] < array[j]:
+                array[j + 1], array[j] = array[j], array[j + 1]
+    return array
+
+
+
+
+print(bubble_sort(bubble_arr))
