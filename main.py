2a38b12ca436c542b8ceda3a5fc47b434fb8b7ff
Nikita <ashtonashtonkachur@gmail.com>
2023-02-13 13:12:46 -0500

    Breadth first search practice

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from collections import deque
 
 # Selection sort algorithm / sorting algorithm, O(N**2) time complexity
 
@@ -5,7 +7,7 @@ selection_sort_arr = [8, 11, -1, 24, 0, 3, 2, -100]
 
 
 def selection_sort(array):
-    for i in range(len(array) - 1):
+    for i in range(len(array)):
         min_value = array[i]
         index = i
         for j in range(i + 1, len(array)):
@@ -19,7 +21,7 @@ def selection_sort(array):
     return array
 
 
-# selection_sort(selection_sort_arr)
+selection_sort(selection_sort_arr)
 
 # Bubble sort algorithm / sorting algorithm O(N**2) time complexity
 bubble_sort_arr = [1, -1, 5, 2, 9, 4]
@@ -38,7 +40,7 @@ def bubble_sort(array):
 
 # Quick sort algorithm / sorting algorithm O(n**2) time complexity
 
-quick_sort_array = [4, 1, 3, 7, 5, 0, 9, 2,]
+quick_sort_array = [4, 1, 3, 7, 5, 0, 9, 2]
 
 
 def quick_sort(array):
@@ -165,6 +167,7 @@ tree_node = TreeNode(left_child, right_child, 10)
 
 def bfs(tree, target):
     if tree:
+        print(tree.value)
         if tree.value == target:
             return tree.value
         else:
@@ -176,42 +179,118 @@ def bfs(tree, target):
 
 
 # print(bfs(tree_node, 100))
-
+#
 
 
 # Depth First Search
-
+#
 # Using a Python dictionary to act as an adjacency list
-graph = {
-  '5': ['3', '7'],
-  '3': ['2', '4'],
-  '7': ['8'],
-  '2': [],
-  '4': ['8'],
-  '8': []
+
+# graph = {
+#   '5': ['3', '7'],
+#   '3': ['2', '4'],
+#   '7': ['8'],
+#   '2': [],
+#   '4': ['8'],
+#   '8': []
+# }
+#
+# visited = set() # Set to keep track of visited nodes of graph.
+#
+#
+# def dfs(graph, node, visited):
+#     if node not in visited:
+#         visited.add(node)
+#         for neibhour in graph[node]:
+#             dfs(graph, neibhour, visited)
+#
+#
+# dfs(graph, '5', visited)
+
+
+# graph = {
+#     'you': ['Alice', 'Bob', 'Claire'],
+#     'Alice': ['Peggy'],
+#     'Claire': ['Jonny', 'Thom'],
+#     'Bob': ['Anuj', 'Peggy'],
+#     'Anuj': [],
+#     'Peggy': [],
+#     'Thom': [],
+#     'Jonny': [],
+# }
+
+# initialize our queue structure
+# search_queue = deque()
+# search_queue += graph['you']
+#
+#
+# def find_mango_seller(structure):
+#     while structure:
+#         person = structure.popleft()
+#         if person_is_seller(person):
+#             print(f'The person {person} is seller!')
+#             return True
+#         else:
+#             structure += graph[person]
+#     return False
+#
+#
+# def person_is_seller(name):
+#     if name == 'Jonny':
+#         return name
+#
+#
+# find_mango_seller(search_queue)
+
+
+
+
+
+my_structure = {
+
+    '8': ['3', '1', '6', '7', '5'],
+    '5': [],
+    '1': [],
+    '3': [],
+    '6': ['9'],
+    '7': ['4'],
+    '4': [],
+    '9': ['2'],
+    '2': []
 }
 
-visited = set() # Set to keep track of visited nodes of graph.
+my_graph = {
+    '9': ['6', '1'],
+    '6': ['8', '21'],
+    '1': ['4', '3'],
+    '8': [],
+    '21': [],
+    '4': [],
+    '3': [],
+}
 
 
-def dfs(graph, node, visited):
-    if node not in visited:
-        print(node)
-        visited.add(node)
-        for neibhour in graph[node]:
-            dfs(graph, neibhour, visited)
+def bfs_practice(graph, vertex):
+    counter = 0
+    # this structure (queue) will help me to check adjacent vertexes.
+    queue = deque()
+    # I need to know which vertex was visited!
+    visited = []
+    # So  i added vertex to visited
+    visited.append(vertex)
+    # and add vertex to be checked in FIFO order(first vertex was found, added, second, added, and them first will out first too
+    queue.append(vertex)
+    while queue:
+        counter += 1
+        vertex = queue.popleft()
+        # print(vertex)
+        for i in graph[vertex]:
+            if i not in visited:
+                visited.append(i)
+                queue.append(i)
+    return counter
 
-
-
-dfs(graph, '5', visited)
-
-
-
-
-
-
-
-
+print(bfs_practice(my_structure, '6'))
 
 
 
